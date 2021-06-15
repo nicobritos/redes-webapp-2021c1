ba9d5cda303d5f5703c19b5957431bdd3bf9b7ba
nicbritos <nicbritos@hotmail.com>
2021-06-15 03:34:40 -0300

    Logger

--- plugins/req.js
+++ plugins/req.js
@@ -1,4 +1,15 @@
+const WinstonLogStash = require('winston3-logstash-transport');
+
 export default (context, inject) => {
     inject('req', context.req);
     context.$req = context.req;
+
+    let pipes = process.winstonLog['_readableState'].pipes;
+    if (!Array.isArray(pipes) || pipes.length === 1) {
+        process.winstonLog.add(new WinstonLogStash({
+            mode: 'tcp',
+            host: '172.31.34.224',
+            port: 28777
+        }));
+    }
 }
